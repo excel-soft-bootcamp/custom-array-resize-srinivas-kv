98fb85c0fa3ee2957baba81de9e98ebb009f6f40
srinivas-kv <srinivaskv88841@gmail.com>
2021-07-13 11:35:56 +0530

    commit

---
+++ Custom_Array/Custom_Array/Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Array
+{
+    class Array
+    {
+        public static void Resize(ref int[] arrayRef, int reSize)
+        {
+            int[] newArray = new int[reSize];
+            for (int i = 0; i < arrayRef.Length; i++)
+            {
+                newArray[i] = arrayRef[i];
+            }
+            arrayRef = newArray;
+            
+        }
+    }
+}
---
+++ Custom_Array/Custom_Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] value = new int[2];
+            value[0] = 10;
+            value[1] = 20;
+            Array.Resize(ref value, value.Length + 2);
+            value[2] = 30;
+            value[3] = 40;
+            Array.Resize(ref value, value.Length + 2);
+            value[4] = 50;
+            value[5] = 60;
+            Array.Resize(ref value, value.Length + 2);
+            value[6] = 70;
+            value[7] = 80;
+            for (int i = 0; i < value.Length; i++)
+            {
+                Console.WriteLine(value[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
